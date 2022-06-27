aace2528aadde965092d800a8a6c83373ddc146f
Bbb <mtg@darkdot.org>
2022-06-27 14:04:14 +0200

    changing subjects

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -186,11 +186,9 @@ if __name__ == "__main__":
     data_collector_web= []
     
     keywords = dict()
-    keywords["SLAM"]                = "SLAM"
-    keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
-    keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\"OR\"image retrieval\""
-    keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
-    keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
+    keywords["Astrophysics"]                = "Astrophysics"
+    keywords["Mathematics"]                 = "math"+"OR"+"math.GN"
+    keywords["AI"] = "Artificial Intelligence"+"OR"+"cs.CV"
 
     for topic,keyword in keywords.items():
  
